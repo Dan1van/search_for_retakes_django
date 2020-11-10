4eea91ddce590d86fa99f59091efdc6388e82f40
dan1van <danushivan@gmail.com>
2020-11-10 14:00:00 +0300

    Models was developed

--- search_retake/admin.py
+++ search_retake/admin.py
@@ -1,32 +1,27 @@
 from django.contrib import admin
 
 from .models import Teacher, Group, Discipline, Retake
-from .forms import RetakeForm
-
-
-@admin.register(Teacher)
-class TeacherAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'name', 'fathers_name')
 
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'course',)
+    list_filter = ('course',)
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'name', 'fathers_name', 'display_discipline',)
+    list_filter = ('disciplines',)
 
 
 @admin.register(Discipline)
 class DisciplineAdmin(admin.ModelAdmin):
-    list_display = ('name', 'semester', 'exam_type')
+    list_display = ('name', 'semester', 'exam_type', 'display_teacher',)
+    list_filter = ('name', 'semester', 'exam_type',)
 
 
 @admin.register(Retake)
 class RetakeAdmin(admin.ModelAdmin):
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-
-        if db_field.name == 'teacher':
-            pass
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
-    # form = RetakeForm
-    list_display = ('discipline', 'group', 'audience', 'date')
+    list_display = ('discipline', 'teacher', 'group', 'audience', 'date',)
+    list_filter = ('discipline', 'teacher', 'group', 'audience', 'date',)
--- search_retake/models.py
+++ search_retake/models.py
@@ -5,6 +5,14 @@ from smart_selects.db_fields import ChainedForeignKey
 
 
 class Group(models.Model):
+    _COURSE_CHOICES = (
+        (1, '1 курс'),
+        (2, '2 курс'),
+        (3, '3 курс'),
+        (4, '4 курс')
+    )
+
+    course = models.IntegerField(choices=_COURSE_CHOICES, verbose_name='Курс', help_text='Выберите курс')
     name = models.CharField(max_length=100, verbose_name='Название', help_text='Введите группу', unique=True)
 
     def __str__(self):
@@ -17,11 +25,16 @@ class Teacher(models.Model):
     last_name = models.CharField(max_length=100, verbose_name='Фамилия', help_text='Введите фамилию преподавателя')
 
     def __str__(self):
-        return f'{self.last_name} {self.name[0]}.'
+        return f'{self.last_name} {self.name[0]}. {self.fathers_name[0]}.'
+
+    def display_discipline(self):
+        return ', '.join([discipline.name for discipline in self.disciplines.all()])
+
+    display_discipline.short_description = 'Дисциплина'
 
 
 class Discipline(models.Model):
-    _SEMESTER_CHOICES = [
+    _SEMESTER_CHOICES = (
         (1, '1 семестр'),
         (2, '2 семестр'),
         (3, '3 семестр'),
@@ -30,18 +43,23 @@ class Discipline(models.Model):
         (6, '6 семестр'),
         (7, '7 семестр'),
         (8, '8 семестр')
-    ]
+    )
 
-    _EXAM_TYPE = [
+    _EXAM_TYPE = (
         (1, 'Зачет'),
         (2, 'Экзамен')
-    ]
+    )
 
     name = models.CharField(max_length=100, verbose_name='Название', help_text='Введите название дисциплины')
     semester = models.IntegerField(choices=_SEMESTER_CHOICES, verbose_name='Семестр', help_text='Выберите семестр')
     exam_type = models.IntegerField(choices=_EXAM_TYPE, verbose_name='Вид контроля', help_text='Выберите вид контроля')
     teachers = models.ManyToManyField(Teacher, related_name='disciplines', verbose_name='Преподаватели',
                                       help_text='Выберите преподавателей')
+
+    def display_teacher(self):
+        return ', '.join([teacher.__str__() for teacher in self.teachers.all()])
+
+    display_teacher.short_description = 'Преподаватели'
 
     def __str__(self):
         return f'{self.name}, {self.semester} сем.'
@@ -52,15 +70,12 @@ class Retake(models.Model):
     teacher = ChainedForeignKey(
         Teacher,
         chained_field='discipline',
-        chained_model_field='disciplines'
+        chained_model_field='disciplines',
+        verbose_name='Преподаватель',
     )
     group = models.ForeignKey(Group, verbose_name='Группа', on_delete=models.CASCADE)
     audience = models.CharField(max_length=5, verbose_name='Номер аудитории', help_text='Введите номер аудитории')
     date = models.DateTimeField(verbose_name='Дата', help_text='Введите дату пересдачи')
-
-    def clean(self):
-        if not self.teacher in self.discipline.teachers:
-            raise ValidationError('Нет преподавателей данной дисциплины')
 
     def __str__(self):
         return f'{self.discipline.name}, {self.group.name}'
